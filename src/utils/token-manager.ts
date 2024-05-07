b0ee3a17a5d8ae95c1b47adaff35556365dffcdd
HelicopterHelicopter <jheel787@gmail.com>
2024-05-08 04:04:40 +0530

    added token-manager utility functions

---
+++ src/utils/token-manager.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { COOKIE_NAME } from './constants';
+
+export const createToken = (id:number, expiresIn:string) => {
+    const payload = {id};
+    const token = jwt.sign(payload,process.env.JWT_SECRET,{
+        expiresIn
+    });
+
+    return token;
+}
+
+export const verifyToken = async (req:Request,res:Response,next:NextFunction) => {
+    const token = req.signedCookies[`${COOKIE_NAME}`];
+    if(!token || token.trim()===""){
+        return res.status(401).json({message:"Token not Received"});
+    }
+
+    return new Promise<void>((resolve,reject)=>{
+        return jwt.verify(token, process.env.JWT_SECRET,(err,success)=>{
+            if(err){
+                reject(err.message);
+                return res.status(401).json({message:"Token Expired"});
+            }else{
+                console.log("Token verified successfully");
+                resolve();
+                res.locals.jwtData = success;
+                return next();
+            }
+        });
+    });
+}
